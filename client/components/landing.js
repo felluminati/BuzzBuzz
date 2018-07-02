ff4ed975b29d2439314e9b7e5267fdb125ddb4be
Cody Fayolle <cfayolle@umich.edu>
2018-07-02 11:57:28 -0500

    style update

--- client/components/landing.js
+++ client/components/landing.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Landing = ({handleClick, isLoggedIn}) => (
+const Landing = ({ handleClick, isLoggedIn }) => (
   <div>
     <h1 className="buzz">Welcome to BuzzBuzz</h1>
     <div className="landing-btn">
@@ -17,26 +17,8 @@ const Landing = ({handleClick, isLoggedIn}) => (
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
+const mapState = null
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+const mapDispatch = null
 
 export default connect(mapState, mapDispatch)(Landing)
-
-/**
- * PROP TYPES
- */
-Landing.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
